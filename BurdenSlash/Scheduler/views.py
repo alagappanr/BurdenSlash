ef099c2be3802f76735a1f43fec36dd57f05141c
ramasamyz <ramasamy.zephyr@gmail.com>
2014-04-06 00:09:23 -0400

    0

--- BurdenSlash/Scheduler/views.py
+++ BurdenSlash/Scheduler/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from django.template import RequestContext
+
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 @login_required
-def home():
+def home(request):
     #home page render
+    return  render_to_response('home.html', context_instance=RequestContext(request));
 
 
 
--- BurdenSlash/urls.py
+++ BurdenSlash/urls.py
@@ -5,9 +5,10 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'BurdenSlash.views.home', name='home'),
+    url(r'^$', 'BurdenSlash.Scheduler.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^login/', 'BurdenSlash.biews.login', name='login'),
+    #url(r'^accounts/login/', 'Scheduler.views.login', name='login'),
+    
 )
